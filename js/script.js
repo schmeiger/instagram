ba0c5b55c186cc3ea3d0f0768eb4848e7f8cc6ce
Neville Bonavia <nbonavia@thinkful.com>
2015-11-30 08:06:13 +0100

    Updated project code so that we're able to successfully interact with instagram.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,19 @@
-function callback(data){
-	alert(data);
-};
-
-
-angular.module('myApp', []).
-	controller('instagramRequest', function($scope, $http){
-
+angular.module('myApp', [])
+    .config(function($httpProvider) {
+        //These have been added for CORS support, but instagram does not support CORS, so we need to use JSONP.
+        $httpProvider.defaults.useXDomain = true;
+        delete $httpProvider.defaults.headers.common['X-Requested-With'];
+    })
+	.controller('instagramRequest', function($scope, $http, $location, $window) {
+        if ($location.path().indexOf('/access_token=') == -1) {
+            var clientId = '948bc6f0f8d5452196436abf6c1cb743';
+            var targetUri = encodeURIComponent('http://127.0.0.1:43048/ANGULAR-JS/students/natali/instagram/index.html');
+            $window.location.href = 'https://instagram.com/oauth/authorize/?client_id=' + clientId + '&redirect_uri=' + targetUri + '&response_type=token&scope=public_content';
+            return;
+        };
+    
+        $scope.code = $location.path().substring(14);
+    
 		$scope.searchSubmit = function() {
 			var searchWord = $scope.data.search;
 			// var url = "https://api.instagram.com/v1/tags/" + searchWord + "/media/recent?client_id=681d0c31ac454bdfac1d543911565da7&callback=callback";
@@ -35,17 +43,17 @@ angular.module('myApp', []).
 
 			// Client ID - 681d0c31ac454bdfac1d543911565da7
 			// Access Token - 96ab943cbd594376b97d91bbe0588cb8
-
-
-			 $http({
-			    url: 'https://api.instagram.com/v1/tags/' + searchWord + '/media/recent?access_token=96ab943cbd594376b97d91bbe0588cb8',
-			    method: 'GET'
-			  })
-			  .then(function(response) {
+            var url = 'https://api.instagram.com/v1/tags/search' +
+                         '?q=' + searchWord + 
+                         '&access_token=' + $scope.code + 
+                         '&callback=JSON_CALLBACK';
+            
+			$http.jsonp(url)
+			  .then(function(json) {
 			    console.log('Success!');
 			    // called when the data is available
 			  },
-			  function(response) {
+			  function(error) {
 			    console.log('Failure :(');
 			    // called when an error occurs or
 			    // the server returns data with an error status
